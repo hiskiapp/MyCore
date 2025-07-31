3b9109d3f2d99b4142c35151f547f53d6074a55c
Hiskia Pratama <hiskianggi@gmail.com>
2025-07-31 11:52:59 +0700

    Add volume and pgadmin to postgres aspire

--- MyCore.AppHost/Program.cs
+++ MyCore.AppHost/Program.cs
@@ -1,6 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres");
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume()
+    .WithPgAdmin();
 
 var database = postgres.AddDatabase("mycoredb");
 
